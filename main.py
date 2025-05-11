3944d9f3a7141f5e4b52856a157392779c73320e
t.barkhozov <t.barkhozov@mts.ai>
2025-05-11 10:55:12 +0300

    add tests

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# import requests
-#
-# response = requests.get("https://playground.learnqa.ru/api/hello")
-# print(response.text)
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/hello")
+print(response.text)
 
 
 print("Hello from Tau")
