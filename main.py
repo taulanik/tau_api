7e48561f1d5310b2b40c6b6fa96b9ee88196ed60
t.barkhozov <t.barkhozov@mts.ai>
2025-05-24 15:55:06 +0300

    add tests

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ import json
 # print("Hello from Tau")
 
 
-json_text = {"messages": [{"message": "This is the first message", "timestamp": "2021-06-04 16:40:53"},
-                          {"message": "And this is a second message", "timestamp": "2021-06-04 16:41:01"}]}
+# json_text = {"messages": [{"message": "This is the first message", "timestamp": "2021-06-04 16:40:53"},
+#                           {"message": "And this is a second message", "timestamp": "2021-06-04 16:41:01"}]}
+#
+# parsed_data = json_text
+# second_message = parsed_data["messages"][1]
+# print(second_message)
 
-parsed_data = json_text
-second_message = parsed_data["messages"][1]
-print(second_message)
+response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+redirect_count = len(response.history)
+final_url = response.url
+print(f"Количество редиректов: {redirect_count}")
+print(f"Конечный URL: {final_url}")
